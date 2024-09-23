b8250254131369f90d20a68e56b6c806e6ea0dc6
Hans Rohit <hansrohit031@gmail.com>
2024-09-23 06:41:58 +0530

    axios crud commit 1

--- axios/src/pages/Sample.jsx
+++ axios/src/pages/Sample.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import { getProjects } from "../services/api";
+import AddComponent from "../components/AddComponent";
 
 const Sample = () => {
-  return;
-  <>
-    <div></div>
-  </>;
+  const [projectdata, setProjectdata] = useState(null);
+  const fetchprojects = async () => {
+    // const {}
+    try {
+      const { data } = await getProjects();
+      setProjectdata(data);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+  console.log(projectdata);
+  useEffect(() => {
+    fetchprojects();
+  }, []);
+  if (!projectdata) {
+    return "No data available";
+  }
+  return (
+    <>
+      <div className="w-full h-full flex flex-row flex-wrap gap-8 justify-center items-center">
+        <div className="w-full h-[10%] flex justify-center items-center">
+          {/* <AddComponent /> */}
+        </div>
+
+        {projectdata.map((data, index) => (
+          <ProjectCard
+            title={data.title}
+            desc={data.desc}
+            key={index}
+            pid={index}
+            cover={data.coverimg}
+            id={data.id}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Sample;
